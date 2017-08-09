16fb6b2b6594cfd2b4404458b9fd97a0797a60f0
KienMN <kienmn97@gmail.com>
2017-08-09 09:40:07 +0700

    Add files via upload

--- Test.py
+++ Test.py
@@ -1,17 +1,11 @@
-n = int(input())
-location = [int(x) for x in input().split()]
-for i in range(0, n):
-    if location[i] == location[0]:
-        mi = location[i + 1] - location[i]
-        ma = location[n - 1] - location[i]
-    elif location[i] == location[n - 1]:
-        mi = location[i] - location[n - 2]
-        ma = location[i] - location[0]
+dic = {}
+n, m = [int(x) for x in input().split()]
+for i in range (0, m):
+    a, b = input().split()
+    if (len(a) > len(b)):
+        dic[a] = b
     else:
-        min1 = abs(location[i] - location[i - 1])
-        min2 = abs(location[i] - location[i + 1])
-        mi = min(min1, min2)
-        max1 = abs(location[i] - location[0])
-        max2 = abs(location[i] - location[n - 1])
-        ma = max(max1, max2)
-    print(mi, ma)
+        dic[a] = a
+lecture = input().split()
+for word in lecture:
+    print(dic[word], end=" ")
