2545be75019557d8c6b0dca4c7f28af9428ec534
KienMN <kienmn97@gmail.com>
2017-08-10 10:15:47 +0700

    Update and rename Test.py to 550A.py

--- 550A.py
+++ 550A.py
@@ -19,11 +19,3 @@ if check:
     print("YES")
 else:
     print("NO")
-
-# n, m = [int(x) for x in input().split()]
-# steps = 0
-# while n < m:
-#     n *= 2
-#     steps += 1
-# steps += (n - m)
-# print(steps)
